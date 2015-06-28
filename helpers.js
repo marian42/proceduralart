f0cbab7724e815b6aaa75e90b4e4727d9e11c425
Marian <mail@marian42.de>
2015-06-28 03:22:12 +0200

    Move helpers to separate file

---
+++ helpers.js
@@ -0,0 +1,94 @@
+
+// http://stackoverflow.com/a/1099670/895589
+function getQueryParams(qs) {
+    qs = qs.split('+').join(' ');
+
+    var params = {},
+        tokens,
+        re = /[?&]?([^=]+)=([^&]*)/g;
+
+    while (tokens = re.exec(qs)) {
+        params[decodeURIComponent(tokens[1])] = decodeURIComponent(tokens[2]);
+    }
+
+    return params;
+}
+
+// http://stackoverflow.com/a/17243070/895589
+
+/* accepts parameters
+ * h  Object = {h:x, s:y, v:z}
+ * OR 
+ * h, s, v
+*/
+function getRGB(h, s, v) {
+    var r, g, b, i, f, p, q, t;
+    if (h && s === undefined && v === undefined) {
+        s = h.s, v = h.v, h = h.h;
+    }
+    i = Math.floor(h * 6);
+    f = h * 6 - i;
+    p = v * (1 - s);
+    q = v * (1 - f * s);
+    t = v * (1 - (1 - f) * s);
+    switch (i % 6) {
+        case 0: r = v, g = t, b = p; break;
+        case 1: r = q, g = v, b = p; break;
+        case 2: r = p, g = v, b = t; break;
+        case 3: r = p, g = q, b = v; break;
+        case 4: r = t, g = p, b = v; break;
+        case 5: r = v, g = p, b = q; break;
+    }
+    return {
+        r: Math.floor(r * 255),
+        g: Math.floor(g * 255),
+        b: Math.floor(b * 255)
+    };
+}
+
+// General random & noise functions
+function getInt(max, pivot) {
+	return (seed % pivot) % max;
+}
+
+function getBool(pivot) {
+	return getInt(2, pivot) == 1;
+}
+
+function getFloat(pivot, from, to) {
+	if (!from)
+		from = 0;
+	if (!to)
+		to = 1;
+	var x = ((seed % pivot) % 100000) / 100000;
+	return from + (to - from) * x;
+}
+
+function getPivot(seed) {
+	var myrng = new Math.seedrandom(seed);
+	return Math.round(myrng() * 100000000);
+}
+
+function simplex(x, y, octaves, lowercap, uppercap) {
+	if (!octaves) octaves = 6;
+	if (!lowercap) lowercap = 0;
+	if (!uppercap) uppercap = 1;
+	
+	var v = 0;
+	for (var i = 1; i <= octaves; i++) {
+		var s = noise.simplex2(x * Math.pow(2, i-1),y * Math.pow(2, i-1)) / 2 + 0.5;
+		v += s * Math.pow(2, -i);
+	}
+	
+	if (v < lowercap)
+		return 0;
+	if (v > uppercap)
+		return 1;
+	return (v - lowercap) / (uppercap - lowercap);
+}
+
+function getColorString(color, alpha) {
+	if (alpha === undefined) 
+		alpha = 1;
+	return 'rgba(' + color.r + ', ' + color.g + ', ' + color.b + ', ' + alpha + ')';
+}
--- script.js
+++ script.js
@@ -29,36 +29,6 @@ var height;
 var imageData;
 var buffer;
 
-/* accepts parameters
- * h  Object = {h:x, s:y, v:z}
- * OR 
- * h, s, v
-*/
-function getRGB(h, s, v) {
-    var r, g, b, i, f, p, q, t;
-    if (h && s === undefined && v === undefined) {
-        s = h.s, v = h.v, h = h.h;
-    }
-    i = Math.floor(h * 6);
-    f = h * 6 - i;
-    p = v * (1 - s);
-    q = v * (1 - f * s);
-    t = v * (1 - (1 - f) * s);
-    switch (i % 6) {
-        case 0: r = v, g = t, b = p; break;
-        case 1: r = q, g = v, b = p; break;
-        case 2: r = p, g = v, b = t; break;
-        case 3: r = p, g = q, b = v; break;
-        case 4: r = t, g = p, b = v; break;
-        case 5: r = v, g = p, b = q; break;
-    }
-    return {
-        r: Math.floor(r * 255),
-        g: Math.floor(g * 255),
-        b: Math.floor(b * 255)
-    };
-}
-
 function setupCanvas() {
 	var c = document.getElementById("myCanvas");
 	ctx = c.getContext("2d");
@@ -105,46 +75,6 @@ function setPixelRGB(x, y, r, g, b, alpha) {
 		buffer[(y * width + x) * 4 + 1] = Math.floor((1 - alpha) * buffer[(y * width + x) * 4 + 1] + alpha * g);
 		buffer[(y * width + x) * 4 + 2] = Math.floor((1 - alpha) * buffer[(y * width + x) * 4 + 2] + alpha * b);
 	}
-}
-
-function getInt(max, pivot) {
-	return (seed % pivot) % max;
-}
-
-function getBool(pivot) {
-	return getInt(2, pivot) == 1;
-}
-
-function getFloat(pivot, from, to) {
-	if (!from)
-		from = 0;
-	if (!to)
-		to = 1;
-	var x = ((seed % pivot) % 100000) / 100000;
-	return from + (to - from) * x;
-}
-
-function getPivot(seed) {
-	var myrng = new Math.seedrandom(seed);
-	return Math.round(myrng() * 100000000);
-}
-
-function simplex(x, y, octaves, lowercap, uppercap) {
-	if (!octaves) octaves = 6;
-	if (!lowercap) lowercap = 0;
-	if (!uppercap) uppercap = 1;
-	
-	var v = 0;
-	for (var i = 1; i <= octaves; i++) {
-		var s = noise.simplex2(x * Math.pow(2, i-1),y * Math.pow(2, i-1)) / 2 + 0.5;
-		v += s * Math.pow(2, -i);
-	}
-	
-	if (v < lowercap)
-		return 0;
-	if (v > uppercap)
-		return 1;
-	return (v - lowercap) / (uppercap - lowercap);
 }
 
 function drawBrightStar(x,y,size) {
