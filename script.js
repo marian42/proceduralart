85432781c2c9abb08c5f36bfc39ecf6492cbdc31
Marian <mail@marian42.de>
2015-07-15 11:58:27 +0200

    Create new images without page reload

--- script.js
+++ script.js
@@ -1,9 +1,23 @@
 seed = getRandomSeed();
 var canvas = document.getElementById("myCanvas");
 
+var btnDiscoverNew = document.getElementById('discovernew');
+
+function discoverNew() {
+	btnDiscoverNew.innerHTML = 'working...';
+	setTimeout(function() {
+		var seed = getRandomSeed();
+		draw(canvas, seed);
+		btnDiscoverNew.innerHTML = 'discover new planet';
+	}, 0);
+
+	return false;
+}
 
 if (getQueryParams(document.location.search).planet !== undefined) {
 	seed = getQueryParams(document.location.search).planet;
+} else {
+	btnDiscoverNew.onclick = discoverNew;
 }
 
 document.getElementById('permalink').href = '/proceduralart/?planet=' + seed;
