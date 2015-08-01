e37f8eabd62fd1118d7792ced6c6b88aaa4e6f0c
Marian <mail@marian42.de>
2015-08-02 00:04:46 +0200

    Better loading indication

--- script.js
+++ script.js
@@ -4,11 +4,13 @@ var canvas = document.getElementById("myCanvas");
 var btnDiscoverNew = document.getElementById('discovernew');
 
 function discoverNew() {
+	document.title = "Working..."
 	btnDiscoverNew.innerHTML = 'working...';
 	setTimeout(function() {
 		var seed = getRandomSeed();
 		draw(canvas, seed);
 		btnDiscoverNew.innerHTML = 'discover new planet';
+		document.title = "Every Planet Procedural";
 		document.getElementById('permalink').href = '/proceduralart/?planet=' + seed;
 	}, 0);
 
@@ -23,4 +25,6 @@ if (getQueryParams(document.location.search).planet !== undefined) {
 
 document.getElementById('permalink').href = '/proceduralart/?planet=' + seed;
 
+document.title = "Working...";
 draw(canvas, seed);
+document.title = "Every Planet Procedural";
